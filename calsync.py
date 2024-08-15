402fa874b1b8111dca57782965cf4c2e89a1b704
Emeric <emeric.million@gmail.com>
2024-08-15 08:58:32 +0200

    Allow args for testing

--- calsync.py
+++ calsync.py
@@ -7,6 +7,7 @@ import pdb
 import re
 import quopri
 from datetime import datetime
+import argparse
 
 from icalendar import Calendar, Event
 import pytz
@@ -27,7 +28,7 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-def process_email(data):
+def process_email(data, ics_directory):
     """
     Processes the incoming email to extract reservation details and create an ICS file.
     """
@@ -49,10 +50,8 @@ def process_email(data):
     start_time = re.search(r'Début : (\d{2}/\d{2}/\d{2}) (\d{2}:\d{2})', text)
     end_time = re.search(r'Fin du voyage : (\d{2}/\d{2}/\d{2}) (\d{2}:\d{2})', text)
     name = re.search(r'Le voyage de (.+?) dans votre', text)
-    if not name:
-        re.search(r'Le voyage de (\w+) est reservé', text)
     car_name = re.search(r'dans votre (.*?) est réservé', text)
-    
+
     if not (start_time and end_time and name):
         logger.warning("Email did not contain all required reservation details.")
         return
@@ -65,7 +64,7 @@ def process_email(data):
     # Create ICS file if valid data is found
     event_name = name.group(1)
     car = car_name.group(1)
-    create_ics_file(event_name, start, end, car, '/var/www/calsync/')
+    create_ics_file(event_name, start, end, car, ics_directory)
     logger.info(f"Processed reservation for {event_name} from {start} to {end}")
 
 def create_ics_file(name, start, end, car, directory):
@@ -96,9 +95,18 @@ def create_ics_file(name, start, end, car, directory):
     logger.info(f"ICS file updated successfully at {ics_path}")
 
 if __name__ == '__main__':
-    # Read email data from stdin
-    data = sys.stdin.read()
-    # for testing purpose, uncomment these lines to read from file
-    # with open('turo_add.eml', 'r') as file:
-    #     data = file.read()    
-    process_email(data)
+    # Setup argument parser
+    parser = argparse.ArgumentParser(description="Process email to generate an ICS calendar event.")
+    parser.add_argument('-f', '--file', type=str, help='Path to the .eml file to process.')
+    parser.add_argument('-p', '--path', type=str, default='/var/www/calsync/', help='Directory path to save the ICS file.')
+    args = parser.parse_args()
+
+    # Read email data from file or stdin
+    if args.file:
+        with open(args.file, 'r') as file:
+            data = file.read()
+    else:
+        data = sys.stdin.read()
+
+    # Process the email and create ICS file
+    process_email(data, args.path)
